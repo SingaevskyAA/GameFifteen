daedfb7312260fb14de379b535faeb6c2a7c3fbc
Алексей <singaevsky.a.a@gmail.com>
2018-05-02 15:52:28 +0300

    IsSolvable and Swap implemented

--- TorrowTechTest/GameCore/Field.cs
+++ TorrowTechTest/GameCore/Field.cs
@@ -17,8 +17,13 @@ namespace TorrowTechTest.GameCore
         {
             Random rnd = new Random();
             var field = Enumerable.Range(1, maxValue).OrderBy(c => rnd.Next()).ToArray();
-
-            //TO DO: Solvable
+            if(!IsSolvable(field))
+            {
+                if (field[0] != maxValue && field[1] != maxValue)
+                    field.Swap(0, 1);
+                else
+                    field.Swap(2, 3);
+            }           
 
             return string.Join(",", field);
         }
@@ -63,17 +68,69 @@ namespace TorrowTechTest.GameCore
         {
             return string.Join(",", field.Cast<int>());
         }
+
+
+        //Проверяет расклад на решаемость 
+        private static bool IsSolvable(int[] field)
+        {
+            var invCounter = 0;
+            int currenRow = 0; 
+            int blankRow = 0;
+
+            for (int i = 0; i < field.Length; i++)
+            {
+                if (i % fiedlDimension == 0)
+                { 
+                    currenRow++;
+                }
+                if (field[i] == maxValue)
+                { 
+                    blankRow = currenRow; 
+                    continue;
+                }
+                for (int j = i + 1; j < field.Length; j++)
+                {
+                    if (field[i] > field[j] && field[j] != maxValue)
+                    {
+                        invCounter++;
+                    }
+                }
+            }
+
+            if (fiedlDimension % 2 == 0)
+            { 
+                if (blankRow % 2 == 0)
+                { 
+                    return invCounter % 2 == 0;
+                }
+                else
+                { 
+                    return invCounter % 2 != 0;
+                }
+            }
+            else
+            { 
+                return invCounter % 2 == 0;
+            }            
+        }
     }
-
-
-    //Расширение для перестановки элементов двумерного массива
+    
     static class Extention
-    {        
+    {       
+        //Расширение для перестановки элементов двумерного массива
         public static void Swap<T>(this T[,] array, int x1, int y1, int x2, int y2)
         {
             T temp = array[x1, y1];
             array[x1, y1] = array[x2, y2];
             array[x2, y2] = temp;
+        }
+
+        //Расширение для перестановки элементов одномерного массива
+        public static void Swap<T>(this T[] array, int x, int y)
+        {
+            T temp = array[x];
+            array[x] = array[y];
+            array[y] = temp;
         }
     }
 }
